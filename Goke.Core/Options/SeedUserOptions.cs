0b09283c38f046fcfb60a47fb2b6a7a4c648c7c2
Goke Oladokun <gokeladokun@hotmail.com>
2026-09-06 22:32:19 +0100

    Add Roles to SeedUserOptions

--- Goke.Core/Options/SeedUserOptions.cs
+++ Goke.Core/Options/SeedUserOptions.cs
@@ -5,6 +5,6 @@
         public required string UserName { get; set; }
         public required string Email { get; set; }
         public string? Password { get; set; }
-        public string? Role { get; set; }
+        public string? Roles { get; set; }
     }
 }
